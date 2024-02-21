9265629e8a21803242ff8c475cadd1df6ea0f05b
Dante451 <ddaciuk@rrc.ca>
2024-02-21 00:34:45 -0600

    Updated the code to prevent taking out more money than the user has

--- bank_transactions.py
+++ bank_transactions.py
@@ -13,19 +13,23 @@ random_balance = random.randint(-1000, 10000)
 balance = round(random_balance, 2)
 
 def deposit_protocol(balance):
-    transaction_input = float(input("Enter amount of transaction:"))
+    transaction_input = float(input("Enter amount of transaction: "))
     balance += transaction_input
     print("*" * 40)
     print("Successfully deposited funds")
-    print("Your new balance is: ", balance)
+    print("Your current balance is: ", balance)
     return balance
 
 def withdraw_protocol(balance):
-    transaction_input = float(input("Enter amount of transaction:"))
-    balance -= transaction_input
-    print("*" * 40)
-    print("Successfully withdrew funds")
-    print("Your new balance is: ", balance)
+    transaction_input = float(input("Enter amount of transaction: "))
+    if transaction_input > balance:
+        print("*" * 40)
+        print("insufficient funds")
+    else:
+        balance -= transaction_input
+        print("*" * 40)
+        print("Successfully withdrew funds")
+        print("Your current balance is: ", balance)
     return balance
 
 def invalid_protocol():
@@ -42,12 +46,11 @@ while True:
     print(f'{"Withdraw: W" : ^40}')
     print(f'{"Quit: Q" : ^40}')
     print("*" * 40)
-
     transaction_selection = input("Enter your selection: ").upper()
     if transaction_selection == "D":
         balance = deposit_protocol(balance)
     elif transaction_selection == "W":
-        balance = withdraw_protocol(balance)
+            balance = withdraw_protocol(balance)
     elif transaction_selection == "Q":
         print("Goodbye")
         break
